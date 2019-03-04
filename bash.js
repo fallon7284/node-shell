edbd0f648c395a320ba5d29509e52a95a237d77a
Brendan Fallon <brendanc.fallon@gmail.com>
2019-03-04 11:36:48 -0600

    first commit node shell

--- bash.js
+++ bash.js
@@ -1,1 +1,18 @@
-console.log('hey')
+process.stdout.write('prompt > ')
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim()
+    const pwd = process.cwd()
+    if (cmd === 'pwd'){
+        process.stdout.write(pwd)
+    }
+    else {
+        process.stdout.write('You typed: ' + cmd)
+        process.stdout.write('\nprompt > ')
+    }
+
+    
+})
+
+
+
+// process.stdin.write('')
