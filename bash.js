4b7107cf625ebe9b1994105fe69351e55a82740d
Brendan Fallon <brendanc.fallon@gmail.com>
2019-06-26 15:59:54 -0400

    added bash.js file

--- bash.js
+++ bash.js
@@ -1,18 +1,5 @@
-process.stdout.write('prompt > ')
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim()
-    const pwd = process.cwd()
-    if (cmd === 'pwd'){
-        process.stdout.write(pwd)
-    }
-    else {
-        process.stdout.write('You typed: ' + cmd)
-        process.stdout.write('\nprompt > ')
-    }
 
-    
-})
+const pwd = require('./pwd')
 
-
-
+pwd()
 // process.stdin.write('')
